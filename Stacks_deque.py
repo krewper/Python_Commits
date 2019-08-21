81b1a54acb7914ae15156edf49cc99fb46e6b232
krewper <adityasethi0033@gmail.com>
2019-08-21 16:01:32 +0530

    Stack operations
    Stack operations using Deque Library

---
+++ Stacks_deque.py
@@ -0,0 +1,18 @@
+from collections import deque
+myStack = deque()
+myStack2 = deque()
+
+
+myStack.append('a')
+myStack.append('b')
+myStack.append('c')
+
+myStack
+deque(['a' ,'b' ,'c'])
+
+myStack2
+deque(['a' ,'b' ,'c'])
+
+
+def myFunc(e):
+  return len(e)
