54f40c001772e8929fd82a20ba9aa011fbc21ede
krewper <adityasethi0033@gmail.com>
2019-08-22 16:05:33 +0530

    Update Stacks_deque.py

--- Stacks_deque.py
+++ Stacks_deque.py
@@ -2,10 +2,15 @@ from collections import deque
 myStack = deque()
 myStack2 = deque()
 
+#function myFunc returns the length of the Stack
+def myFunc(e):
+  print (len(e))
+
 
 myStack.append('a')
 myStack.append('b')
 myStack.append('c')
+myFunc(myStack)
 
 myStack
 deque(['a' ,'b' ,'c'])
@@ -14,5 +19,3 @@ myStack2
 deque(['a' ,'b' ,'c'])
 
 
-def myFunc(e):
-  return len(e)
