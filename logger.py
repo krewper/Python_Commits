66afc3b7f2bcfd1de56fc7367de5c7d5423402e0
krewper <adityasethi0033@gmail.com>
2019-08-23 07:05:47 +0000

    Logging function running in Python
    Python logging concepts Implementation

---
+++ logger.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(filename = "logging_input.log", format='%(asctime)s %(message)s', filemode='w')
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
+
+logger.debug("Harmless debug Message")
+logger.info("Just an information")
+logger.warning("Its a Warning")
+logger.error("Did you try to divide by zero")
+logger.critical("Internet is down")
