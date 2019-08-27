19e22f925d3947495b10e35c6cae5804f3c91286
krewper <adityasethi0033@gmail.com>
2019-08-27 09:28:19 +0530

    Neural Networks Implementation using Python
    Single neuron Neural Net achieved implementing python library NumPy

--- neural_net.py
+++ neural_net.py
@@ -7,27 +7,27 @@ class NeuralNetwork():
         
         self.weight_matrix = 2 * random.random((3, 1)) - 1
         
-        def tanh(self, x):
-            return tanh(x)
+    def tanh(self, x):
+        return tanh(x)
         
-        def tanh_derivative(self, x):
-            return 1.0 - tanh(x) ** 2
+    def tanh_derivative(self, x):
+        return 1.0 - tanh(x) ** 2
         
-        def forward_propagation(self, inputs):
-            return self.tanh(dot(inputs, self.weight_matrix))
+    def forward_propagation(self, inputs):
+        return self.tanh(dot(inputs, self.weight_matrix))
         
-        def train(self, train_inputs, train_outputs, num_train_iterations):
+    def train(self, train_inputs, train_outputs, num_train_iterations):
             
-            for iteration in range(num_train_iterations):
-                output = self.forward_propagation(train_inputs)
+        for iteration in range(num_train_iterations):
+            output = self.forward_propagation(train_inputs)
                 
-                error = train_outputs - output
+            error = train_outputs - output
                 
                 
-                adjustment = dot(train_inputs.T, error *
+            adjustment = dot(train_inputs.T, error *
                              self.tanh_derivative(output))
                 
-                self.weight_matrix +=adjustment
+            self.weight_matrix +=adjustment
             
 if __name__ == "__main__":
     
@@ -39,6 +39,7 @@ if __name__ == "__main__":
     train_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
     train_outputs = array([[0,1,1,0]]).T
     
+    neural_network.tanh(50)
     neural_network.train(train_inputs, train_outputs, 10000)
     
     print ('New weights after training', 10000)
@@ -46,6 +47,3 @@ if __name__ == "__main__":
     
     print ("Testing network on new examples ->")
     print (neural_network.forward_propagation(array([1, 0, 0])))
-    
-    
-                
