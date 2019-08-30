e825ea26bcaebf5e7987587170fa0d122ec72cd6
krewper <adityasethi0033@gmail.com>
2019-08-30 17:32:25 +0530

    Update gui2.py

--- gui2.py
+++ gui2.py
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     grid.addWidget(QPushButton("Button two"),0,1)
     grid.addWidget(QPushButton("Button three"),1,0)
     grid.addWidget(QPushButton("Button four"),1,1)
-    grid.addWidget(QSlider("Move slider"))
 
     w.show()
     sys.exit(app.exec_())
