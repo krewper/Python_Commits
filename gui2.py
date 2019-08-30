22a3958cc2ef9521935d78b5448f9388c267cbce
krewper <adityasethi0033@gmail.com>
2019-08-30 17:26:18 +0530

    GUI Widgets
    Widget testing in Python

---
+++ gui2.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import *
+
+if __name__ == "__main__":
+    app = QApplication([])
+
+    w = QWidget()
+    
+    grid = QGridLayout(w)
+    grid.addWidget(QPushButton("Button one"),0,0)
+    grid.addWidget(QPushButton("Button two"),0,1)
+    grid.addWidget(QPushButton("Button three"),1,0)
+    grid.addWidget(QPushButton("Button four"),1,1)
+    grid.addWidget(QSlider("Move slider"))
+
+    w.show()
+    sys.exit(app.exec_())
+    
