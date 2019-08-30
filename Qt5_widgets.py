21c7d9c1a3ca0501349ad1020225139c4fd963e5
krewper <adityasethi0033@gmail.com>
2019-08-30 17:30:29 +0530

    GUI Widget
    Hello World Implementation in Python

---
+++ Qt5_widgets.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QApplication, QPushButton, QSlider
+
+
+
+app = QApplication([])
+app.setStyle('Fusion')
+palette = QPalette()
+palette.setColor(QPalette.ButtonText, Qt.red)
+app.setPalette(palette)
+button = QPushButton('Hello World')
+
+button.show()
+app.exec_()
