4b41f92261c9b9599b9f5a03e3fd69c147aeeb5e
krewper <adityasethi0033@gmail.com>
2019-09-03 17:47:48 +0530

    Image Detection using ImageAI
    Object Detection using ImageAI library

---
+++ Img_det.py
@@ -0,0 +1,18 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsYOLOv3()
+detector.setModelPath(os.path.join(execution_path, "car1.jpg"))
+detector.loadModel()
+
+custom_objects = detector.CustomObjects(car=True, motorcycle=True)
+detections = detector.detectCustomObjectsFromImage(custom_objects=custom_objects, input_image=os.path.join(execution_path , "car1.jpg"), output_image_path=os.path.join(execution_path , "image3custom.jpg"), minimum_percentage_probability=30)
+
+
+for eachObject, eachObjectPath in zip(detections, objects_path):
+    print(eachObject["name"] , " : " , eachObject["percentage_probability"], " : ", eachObject["box_points"] )
+    print("Object's image saved in " + eachObjectPath)
+    print("--------------")
