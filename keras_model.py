a9763423231d35a92f73f9be0ec707644606bcdf
krewper <adityasethi0033@gmail.com>
2019-09-05 18:22:43 +0530

    Keras Library
    Implementation of Keras library

---
+++ keras_model.py
@@ -0,0 +1,50 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.datasets import cifar10
+
+# Generate dummy data
+import numpy as np
+from keras.datasets import cifar10
+from keras.datasets import imdb
+
+
+
+keras.initializers.Initializer( )
+keras.initializers.Ones(    )
+
+x_train = np.random.random((1000, 20))
+y_train = keras.utils.to_categorical(np.random.randint(10, size=(1000, 1)), num_classes=10)
+x_test = np.random.random((100, 20))
+y_test = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
+                                                      num_words=None,
+                                                      skip_top=0,
+                                                      maxlen=None,
+                                                      seed=113,
+                                                      start_char=1,
+                                                      oov_char=2,
+                                                      index_from=3))
+
+model = Sequential()
+# Dense(64) is a fully-connected layer with 64 hidden units.
+# in the first layer, you must specify the expected input data shape:
+# here, 20-dimensional vectors.
+model.add(Dense(64, activation='relu', input_dim=20))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy',
+              optimizer=sgd,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          epochs=20,
+          batch_size=128)
+score = model.evaluate(x_test, y_test, batch_size=128)
